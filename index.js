36b935996f7a9efb6e2ec9633a7872a5df779d02
kode <105132310+kodescripts@users.noreply.github.com>
2023-08-15 19:05:31 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).send('Missing URL parameter');
+  }
+
+  try {
+    const response = await axios.get(url);
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
